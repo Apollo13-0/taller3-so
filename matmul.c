2d9301aa8054f0338a0ff9951a9d4528fc4e841f
Ignacio Calderón <53590021+Apollo13-0@users.noreply.github.com>
2024-11-08 15:29:00 -0600

    Update matmul.c

--- matmul.c
+++ matmul.c
@@ -13,6 +13,15 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    // Asegurarse de que el número de procesos sea igual a N
+    if (size != N) {
+        if (rank == 0) {
+            printf("El número de procesos debe ser igual a %d\n", N);
+        }
+        MPI_Finalize();
+        return 0;
+    }
+
     // Inicializar las matrices en el proceso raíz
     if (rank == 0) {
         printf("Matriz A:\n");
@@ -38,18 +47,7 @@ int main(int argc, char **argv) {
     MPI_Bcast(B, N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Enviar una fila de la matriz A a cada proceso
-    if (rank == 0) {
-        for (int i = 1; i < size; i++) {
-            MPI_Send(A[i], N, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
-        // El proceso 0 también trabaja en su propia fila (la primera fila)
-        for (int j = 0; j < N; j++) {
-            local_row[j] = A[0][j];
-        }
-    } else {
-        // Los procesos reciben su fila de A
-        MPI_Recv(local_row, N, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    MPI_Scatter(A, N, MPI_INT, local_row, N, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Cada proceso realiza la multiplicación de la fila local por la matriz B
     for (int j = 0; j < N; j++) {
@@ -60,18 +58,10 @@ int main(int argc, char **argv) {
     }
 
     // Recoger los resultados en la matriz C en el proceso raíz
+    MPI_Gather(result_row, N, MPI_INT, C, N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Imprimir la matriz resultante en el proceso raíz
     if (rank == 0) {
-        // El proceso 0 almacena su resultado en la primera fila de C
-        for (int j = 0; j < N; j++) {
-            C[0][j] = result_row[j];
-        }
-
-        // Recibir las filas calculadas por otros procesos
-        for (int i = 1; i < size; i++) {
-            MPI_Recv(C[i], N, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-
-        // Imprimir el resultado final
         printf("\nMatriz Resultante C (AxB):\n");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -79,9 +69,6 @@ int main(int argc, char **argv) {
             }
             printf("\n");
         }
-    } else {
-        // Los procesos no raíz envían su fila calculada de C al proceso 0
-        MPI_Send(result_row, N, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
